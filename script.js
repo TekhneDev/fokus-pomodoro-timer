9075c9e4582657011f2a466f145a152a807a01dc
Carla Mariana <carlamab2808@outlook.com>
2024-05-26 23:18:05 -0300

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+})
+
+//elemento.addEventListener(evento, callback);
+/*
+    elemento: É o elemento HTML ao qual queremos associar o evento.
+    evento: É uma string que representa o tipo de evento que desejamos capturar.
+    callback: É a função que será chamada quando o evento ocorrer.
+*/
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+})
