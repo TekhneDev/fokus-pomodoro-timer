6bf5e336463272e0f4a47f93412454696d82c71b
Carla Mariana <carlamab2808@outlook.com>
2024-05-28 23:40:07 -0300

    teste

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+//
+const botaoIniciar = document.querySelector('.app__card-primary-button');
+// Ver esses abaixo ??
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title')
+
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
@@ -21,3 +28,9 @@ curtoBt.addEventListener('click', () => {
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
 })
+
+/*
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900; 
+*/
