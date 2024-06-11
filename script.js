ae6c15a8a9c21ede5016a1d5519c1b7e635f3fc1
Carla Mariana <carlamab2808@outlook.com>
2024-06-10 21:38:31 -0300

    prettier

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-const html = document.querySelector('html')
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo')
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
 //
-const botaoIniciar = document.querySelector('.app__card-primary-button')
-const displayTempo = document.querySelector('#timer')
-const banner = document.querySelector('.app__image')
-const titulo = document.querySelector('.app__title')
-const botoes = document.querySelectorAll('.app__card-button')
-const startPauseBt = document.querySelector('#start-pause')
-const musicaFocoInput = document.querySelector('#alternar-musica')
-const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const botaoIniciar = document.querySelector(".app__card-primary-button");
+const displayTempo = document.querySelector("#timer");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const iniciarOuPausarBt = document.querySelector("#start-pause")
+//
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
+const audioPlay = new Audio("/sons/play.wav");
+const audioPausa = new Audio("/sons/pause.mp3");
+const audioTempoFinalizado = new Audio("./sons/beep.mp3");
 
-let tempoDecorridoEmSegundos = 5
-let intervaloId = null
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
-musica.loop = true
+musica.loop = true;
 
-musicaFocoInput.addEventListener('change', () => {
-    if(musica.paused) {
-        musica.play()
-    }
-    else {
-        musica.pause()
-    }
-})
+musicaFocoInput.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
-// Implementar ...
-// Ver aúdios corretamente
-
-focoBt.addEventListener('click', () => {
-    alterarContexto('foco')
-    focoBt.classList.add('active')
-})
+focoBt.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBt.classList.add("active");
+});
 
 //elemento.addEventListener(evento, callback);
 /*
@@ -41,76 +42,78 @@ focoBt.addEventListener('click', () => {
     callback: É a função que será chamada quando o evento ocorrer.
 */
 
-curtoBt.addEventListener('click', () => {
-    alterarContexto('descanso-curto')
-    curtoBt.classList.add('active')
-})
+curtoBt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
+});
 
-longoBt.addEventListener('click', () => {
-    alterarContexto('descanso-longo')
-    longoBt.classList.add('active')
-})
+longoBt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
+});
 
 function alterarContexto(contexto) {
-    botoes.forEach(function(contexto){
-        contexto.classList.remove('active')
-    })
-    html.setAttribute('data-contexto', contexto)
-    // Por que que aqui foi usado essas aspas ` ` DIFERENTES?
-    banner.setAttribute('src', `./imagens/${contexto}.png`)
-    switch (contexto) {
-        case "foco":
-            // o metodo innerHTML tambem é bom para fazer LISTAS
-            titulo.innerHTML = `
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", contexto);
+  // Por que que aqui foi usado essas aspas ` ` DIFERENTES?
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      // o metodo innerHTML tambem é bom para fazer LISTAS
+      titulo.innerHTML = `
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
-            break;
-        
-        case "descanso-curto":
-            titulo.innerHTML = `
+            `;
+      break;
+
+    case "descanso-curto":
+      titulo.innerHTML = `
             Quer dar uma respirada? <br>
                 <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            `
-            break;
-        
-        case "descanso-longo":
-            titulo.innerHTML = `
+            `;
+      break;
+
+    case "descanso-longo":
+      titulo.innerHTML = `
             Hora de voltar à superfície <br>
                 <strong class="app__title-strong">Faça uma pausa longa.</strong>
-            `
+            `;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 const contagemRegressiva = () => {
-    if(tempoDecorridoEmSegundos <= 0) {
-        zerar()
-        alert('Tempo finalizado!')
-        return
-    }
-    tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
-}
+  if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoFinalizado.play(); // áudio executado quando cronômetro finalizar
+    alert("Tempo finalizado!");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("Temporizador: " + tempoDecorridoEmSegundos);
+};
 
-startPauseBt.addEventListener('click', iniciarOuPausar)
-//
-// testestestes
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
 function iniciarOuPausar() {
-    if(intervaloId) {
-        zerar()
-        return
-    }
-    // 1000 porque a funcao recebe o valor em milissegundos 
-    intervaloId = setInterval(contagemRegressiva, 1000)
+  if (intervaloId) {
+    audioPausa.play(); // áudio executado quando cronômetro for pausado
+    zerar();
+    return;
+  }
+  audioPlay.play(); // áudio executado quando cronômetro iniciar
+  intervaloId = setInterval(contagemRegressiva, 1000);
 }
 
 function zerar() {
-    clearInterval(intervaloId)
-    intervaloId = null
+  clearInterval(intervaloId);
+  intervaloId = null;
 }
+
 /*
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
